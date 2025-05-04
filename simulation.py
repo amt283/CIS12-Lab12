cadbc5ffcb45be9666b318c383a0dd34aef05268
amt954 <athompson283@gmail.com>
2025-05-03 22:55:03 -0700

    Updated planet values

--- simulation.py
+++ simulation.py
@@ -32,10 +32,10 @@ if __name__ == '__main__':
     the_sun = Sun("SOL", 5000, 10000000, 5800,0,0)
     solar_system.add_sun(the_sun)
 
-    earth = Planet("EARTH", 47.5, 1, 25, 5.0, 200.0, -15, 40,"green")
+    earth = Planet("EARTH", 47.5, 100, 25, 5.0, 200.0, -15, 40,"green")
     solar_system.add_planet(earth)
 
-    mars = Planet("MARS", 40.5, .1, 62, 10.0, 125.0, -15, 40,"red")
+    mars = Planet("MARS", 40.5, 50, 62, 10.0, 125.0, -15, 40,"red")
     solar_system.add_planet(mars)
 
     simulation = Simulation(solar_system, 500, 500, 2000)
